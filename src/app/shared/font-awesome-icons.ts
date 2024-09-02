8d05964e513b064603dd418a05516af1c63a0aa7
GustavoGuerato <guerato.gustavo14@gmail.com>
2024-09-02 19:46:57 -0300

    some changes in app.components.ts

---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,8 @@
+import {
+  faCircle,
+  faCirclePause,
+  faCirclePlay,
+  IconDefinition,
+} from '@fortawesome/free-regular-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [faCirclePlay];
